a0fe0eb9b683296b6ba2dba8012cf52f7db65ed0
pellytw <marcelo.eduardo.gonzalez@gmail.com>
2015-07-09 19:59:56 -0300

    cambios vista jugadores y comenzando a armar el sed

---
+++ extras/armando seed.rb
@@ -0,0 +1,99 @@
+#Posiciones
+Posicion.create(nombre: 'ARQ')
+Posicion.create(nombre: 'DEF')
+Posicion.create(nombre: 'VOL')
+Posicion.create(nombre: 'DEL')
+
+#Clubes
+Club.create(:nombre: 'ALDOSIVI')
+Club.create(:nombre: 'ARGENTINOS JUNIORS')
+Club.create(:nombre: 'ARSENAL')
+Club.create(:nombre: 'ATLÉTICO DE RAFAELA')
+Club.create(:nombre: 'BANFIELD')
+Club.create(:nombre: 'BELGRANO')
+Club.create(:nombre: 'BOCA JUNIORS')
+Club.create(:nombre: 'COLÓN')
+Club.create(:nombre: 'CRUCERO DEL NORTE')
+Club.create(:nombre: 'DEFENSA Y JUSTICIA')
+Club.create(:nombre: 'ESTUDIANTES LP.')
+Club.create(:nombre: 'GIMNASIA Y ESGRIMA LP.')
+Club.create(:nombre: 'GODOY CRUZ')
+Club.create(:nombre: 'HURACÁN')
+Club.create(:nombre: 'INDEPENDIENTE')
+Club.create(:nombre: 'LANÚS')
+Club.create(:nombre: 'NEWELLS OLD BOYS')
+Club.create(:nombre: 'NUEVA CHICAGO')
+Club.create(:nombre: 'OLIMPO')
+Club.create(:nombre: 'QUILMES')
+Club.create(:nombre: 'RACING')
+Club.create(:nombre: 'RIVER PLATE')
+Club.create(:nombre: 'ROSARIO CENTRAL')
+Club.create(:nombre: 'SAN LORENZO')
+Club.create(:nombre: 'SAN MARTÍN SAN JUAN')
+Club.create(:nombre: 'SARMIENTO')
+Club.create(:nombre: 'TEMPERLEY')
+Club.create(:nombre: 'TIGRE')
+Club.create(:nombre: 'UNIÓN')
+Club.create(:nombre: 'VÉLEZ SARSFIELD')
+
+#Jugadores
+#ALDOSIVI
+
+#ARGENTINOS JUNIORS
+
+#ARSENAL SARANDÍ
+
+#ATLÉTICO DE RAFAELA
+
+#BANFIELD
+
+#BELGRANO
+
+#BOCA JUNIORS
+
+#COLÓN
+
+#CRUCERO DEL NORTE
+
+#DEFENSA Y JUSTICIA
+
+#ESTUDIANTES LP.
+
+#GIMNASIA Y ESGRIMA LP.
+
+#GODOY CRUZ
+
+#HURACÁN
+
+#INDEPENDIENTE
+
+#LANÚS
+
+#NEWELL'S OLD BOYS
+
+#NUEVA CHICAGO
+
+#OLIMPO
+
+#QUILMES
+
+#RACING
+
+#RIVER PLATE
+
+#ROSARIO CENTRAL
+
+#SAN LORENZO
+
+#SAN MARTÍN SAN JUAN
+
+#SARMIENTO
+
+#TEMPERLEY
+
+#TIGRE
+
+#UNIÓN
+
+#VÉLEZ SARSFIELD
+
